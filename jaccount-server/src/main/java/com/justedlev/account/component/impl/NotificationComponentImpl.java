a4b444287845c4d0983cfb15c351fe0f0d50bb06
Justedlev <edyardlyk@gmail.com>
2023-02-25 14:09:22 +0200

    -

--- jaccount-server/src/main/java/com/justedlev/account/component/impl/NotificationComponentImpl.java
+++ jaccount-server/src/main/java/com/justedlev/account/component/impl/NotificationComponentImpl.java
@@ -3,6 +3,7 @@ package com.justedlev.account.component.impl;
 import com.justedlev.account.client.EndpointConstant;
 import com.justedlev.account.component.NotificationComponent;
 import com.justedlev.account.constant.MailSubjectConstant;
+import com.justedlev.account.constant.MailTemplateConstant;
 import com.justedlev.account.properties.JAccountProperties;
 import com.justedlev.account.repository.entity.Account;
 import com.justedlev.account.repository.entity.Contact;
@@ -24,26 +25,36 @@ public class NotificationComponentImpl implements NotificationComponent {
 
     @Override
     public void sendConfirmationEmail(Account account) {
-        var confirmationLink = UriComponentsBuilder.fromHttpUrl(properties.getService().getHost())
-                .path(EndpointConstant.V1_ACCOUNT_CONFIRM)
-                .path("/" + account.getActivationCode())
-                .build().toUriString();
-        var content = Map.of(
-                "{FULL_NAME}", account.getNickname(),
-                "{CONFIRMATION_LINK}", confirmationLink,
-                "{BEST_REGARDS_FROM}", properties.getService().getName()
-        );
-        var recipient = account.getContacts()
+        var content = buildContent(account);
+        var recipient = getRecipient(account);
+        var subject = String.format(MailSubjectConstant.CONFIRMATION, properties.getService().getName());
+        var mail = SendTemplateMailRequest.builder()
+                .templateName(MailTemplateConstant.ACCOUNT_CONFIRMATION)
+                .recipient(recipient)
+                .subject(subject)
+                .content(content)
+                .build();
+        notificationQueue.sendEmail(mail);
+    }
+
+    private String getRecipient(Account account) {
+        return account.getContacts()
                 .stream()
                 .min(Comparator.comparing(Contact::getCreatedAt))
                 .map(Contact::getEmail)
                 .orElseThrow(() -> new EntityNotFoundException("Cant find contact for account " + account.getId()));
-        var mail = SendTemplateMailRequest.builder()
-                .recipient(recipient)
-                .subject(String.format(MailSubjectConstant.CONFIRMATION, properties.getService().getName()))
-                .templateName("account-confirmation")
-                .content(content)
-                .build();
-        notificationQueue.sendEmail(mail);
+    }
+
+    private Map<String, String> buildContent(Account account) {
+        var confirmationLink = UriComponentsBuilder.fromHttpUrl(properties.getService().getHost())
+                .path(EndpointConstant.V1_ACCOUNT_CONFIRM)
+                .path("/" + account.getActivationCode())
+                .build().toUriString();
+
+        return Map.of(
+                "{FULL_NAME}", account.getNickname(),
+                "{CONFIRMATION_LINK}", confirmationLink,
+                "{BEST_REGARDS_FROM}", properties.getService().getName()
+        );
     }
 }
